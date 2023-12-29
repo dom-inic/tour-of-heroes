b825572240f53e5c4c91113fed40f9b002d04a55
dom-inic <dominicnyambane22@gmail.com>
2023-12-29 16:56:01 +0300

    hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
 
@@ -15,9 +15,20 @@ import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
     imports: [CommonModule, FormsModule, NgFor, NgIf, UpperCasePipe, HeroDetailComponent]
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) {}
 }
