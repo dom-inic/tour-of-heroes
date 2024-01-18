8e5cc48dae2a3e12dceb43cd9abe6bfea3a285e5
dom-inic <dominicnyambane22@gmail.com>
2024-01-18 13:26:12 +0300

    dashboard, routing, heroes/detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HeroesComponent } from "./heroes/heroes.component";
 import { MessagesComponent } from "./messages/messages.component";
 
@@ -9,7 +10,7 @@ import { MessagesComponent } from "./messages/messages.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeroesComponent, MessagesComponent]
+    imports: [CommonModule, RouterOutlet, HeroesComponent, MessagesComponent, RouterModule]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,6 +6,7 @@ import { HeroService } from '../hero.service';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
 import { MessageService } from '../message.service';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
@@ -13,22 +14,17 @@ import { MessageService } from '../message.service';
     standalone: true,
     templateUrl: './heroes.component.html',
     styleUrl: './heroes.component.css',
-    imports: [CommonModule, FormsModule, NgFor, NgIf, UpperCasePipe, HeroDetailComponent]
+    imports: [CommonModule, FormsModule, NgFor, NgIf, UpperCasePipe, HeroDetailComponent, RouterModule]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+
   // injecting heroservice by adding a private heroservice parameter to the HeroesComponent constructor
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
    // lifecycle hook. Angular calls the hook at an appropriate time after constructing a HeroesComponent instance
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
